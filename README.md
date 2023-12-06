2e2f8ebc56586f798095150a81a6a91dcbc49ff7
tor first <victoriah.first@gmail.com>
2023-12-05 22:26:02 -0600

    updated index.js file

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# readme-generator
+# test
+
+![License](https://img.shields.io/badge/License-MIT-blue)
+
+## Description
+test
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+test
+
+## Usage
+test
+
+## Credits
+test
+
+## License
+MIT
+
+## Questions
+Github: [tes](https://github.com/tes)
+
+Email: tes
--- index.js
+++ index.js
@@ -1,13 +1,104 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a brief description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Who all contributed to this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you run unit tests for this project?',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+
+];
+
+const generateREADME = ({ title, description, installation, usage, credits, license, username, email }) => 
+`# ${title}
+
+![License](https://img.shields.io/badge/License-${license}-blue)
+
+## Description
+${description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Credits
+${credits}
+
+## License
+${license}
+
+## Questions
+Github: [${username}](https://github.com/${username})
+
+Email: ${email}`
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => writeFile(fileName, data);
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    inquirer
+        .prompt(questions)
+        .then((answers) => writeToFile('README.md', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
 
 // Function call to initialize app
 init();
+
+
